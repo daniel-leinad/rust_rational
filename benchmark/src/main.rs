0955ef4571e9ff18dccf4a68adc5f21a334002ca
daniel <fundan.fundan@gmail.com>
2024-03-04 18:09:31 +0300

    Add benchmarks for tests

---
+++ benchmark/src/main.rs
@@ -0,0 +1,19 @@
+mod generated;
+
+use std::time::Instant;
+
+fn main() {
+    measure_func(generated::using_parse_int, "using_parse_int");
+    measure_func(generated::using_parse_float, "using_parse_float");
+    measure_func(generated::using_macro_int, "using_macro_int");
+    measure_func(generated::using_macro_float, "using_macro_float");
+}
+
+fn measure_func(mut func: impl FnMut() -> (), name: &str) {
+    let start_time = Instant::now();
+    for _ in 0..100 {
+        func();
+    }
+    let elapsed = start_time.elapsed();
+    println!("benchmark {name}: {elapsed:?}")
+}
